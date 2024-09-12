513569de30285b2fe445a7d3a74d8f71e292d8bf
Aries Gunawan <aries.thio@hotmail.com>
2024-09-12 17:16:07 +0200

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { WalletSelector } from "./WalletSelector";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+export function Header() {
+  return (
+    <div className="flex items-center justify-between px-10 py-10 max-w-screen-xl mx-auto w-full flex-wrap">
+      <h1 className="display">AptosProp</h1>
+
+      <div className="flex gap-2 items-center flex-wrap">
+        <Link className={buttonVariants({ variant: "link" })} href={"/new-listings"}>
+          New Listings
+        </Link>
+        <Link className={buttonVariants({ variant: "link" })} href={"/marketplace"}>
+          Marketplace
+        </Link>
+        <Link className={buttonVariants({ variant: "link" })} href={"/portfolio"}>
+          My Portfolio
+        </Link>
+        <Link className={cn(buttonVariants({ variant: "green" }), "mx-10")} href={"/tokenize"}>
+          Tokenize Property
+        </Link>
+      </div>
+
+      <WalletSelector />
+
+    </div>
+  );
+}
